8324f95dc130757361747a8d735a10fa7482ae85
karendcl <karen.canterolopez@gmail.com>
2024-03-13 15:22:08 -0400

    added example of lexer types

---
+++ src/lexer/utils/token_types.py
@@ -0,0 +1,9 @@
+import src.cmp.grammar as grammar
+
+token_types = {
+    'num' : grammar.num,
+    '+': grammar.plus,
+    ',': grammar.comma,
+    ';': grammar.semi_colon,
+#    y asi sucesivamente
+}
