48a51064bd9c3da9b5c89af747efa3b4cc583646
karendcl <karen.canterolopez@gmail.com>
2024-03-21 18:24:07 -0400

    grammar + ast done

--- src/cmp/grammar.py
+++ src/cmp/grammar.py
@@ -14,11 +14,11 @@ end_extended = G.NonTerminal("<end-extended>")
 param_list, param = G.NonTerminals("<param-list> <param>")
 arg_declaration = G.NonTerminal("<arg-declaration>")
 let_exp, assign_var, assign_list = G.NonTerminals("<let-exp> <assign-var> <assign-list>")
+mutate_var = G.NonTerminal("<mutate-var>")
 conditional, condition = G.NonTerminals("<conditional> <condition>")
 elif_block = G.NonTerminal("<elif-block>")
 while_block = G.NonTerminal("<while-block>")
 for_exp = G.NonTerminal("<for-exp>")
-boolean = G.NonTerminal("<boolean>")
 type_dec = G.NonTerminal("<type-dec>")
 term, factor, atom, k = G.NonTerminals("<term> <factor> <atom> <k>")
 statement = G.NonTerminal("<statement>")
@@ -47,6 +47,7 @@ let, inx = G.Terminals("let in")
 equal, mut = G.Terminals("= :=")
 ifx, elsex, elifx, whilex, forx, function = G.Terminals("if else elif while for function")
 leq, less, equals, geq, greater, neq = G.Terminals("<= < == >= > !=")
+sqrt, cos, sin, expon, log, rand = G.Terminals("sqrt cos sin expon log rand")
 plus, minus, star, div, mod, pow, num, notx, andx, orx = G.Terminals("+ - * / % ^ num ! & |")
 typex, new, inherits, isx, asx = G.Terminals("type new inherits is as")
 protocol, extends = G.Terminals("protocol extends")
@@ -64,29 +65,34 @@ printx = G.Terminal("print")
 
 program %= statement, lambda h, s: ProgramNode(s[1])
 
-statement %= exp + semi_colon, lambda h, s: s[1]
-statement %= exp_block, lambda h, s: s[1]
-statement %= def_func, lambda h, s: s[1]
-statement %= let_exp, lambda h, s: s[1]
-statement %= def_protocol, lambda h, s: s[1]
-statement %= while_block, lambda h, s: s[1]
-statement %= for_exp, lambda h, s: s[1]
-statement %= conditional, lambda h, s: s[1]
-statement %= print_exp, lambda h, s: s[1]
+statement %= def_protocol + statement, lambda h, s: s[1]
+statement %= type_dec + statement, lambda h, s: s[1]
+statement %= def_func + statement, lambda h, s: s[1]
+statement %= exp_or_block , lambda h, s: s[1]
+# statement %= G.Epsilon, lambda h, s: None
 
-exp %= instance, lambda h, s: s[1]
+
+exp %= let_exp, lambda h, s: s[1]
+exp %= conditional, lambda h, s: s[1]
+exp %= while_block, lambda h, s: s[1]
+exp %= for_exp, lambda h, s: s[1]
+exp %= print_exp, lambda h, s: s[1]
 exp %= func_call, lambda h, s: s[1]
-exp %= string_exp, lambda h, s: s[1]
+exp %= instance, lambda h, s: s[1]
+exp %= term, lambda h, s: s[1]
+exp %= mutate_var, lambda h, s: s[1]
+
 #-------------------------------------------
 
-exp_or_block %= exp, lambda h, s: s[1]
+exp_or_block %= exp + semi_colon, lambda h, s: s[1]
 exp_or_block %= exp_block, lambda h, s: s[1]
 
 def_protocol %= protocol + idx + curly_o + method_declarations + curly_c, lambda h, s: ClassDeclarationNode(s[2], s[4], s[1], None)
 def_protocol %= protocol + idx + extends + idx + curly_o + method_declarations + curly_c, lambda h, s: ClassDeclarationNode(s[2], s[4], s[1], s[3])
-def_method %= idx + opar + param_list + cpar + colon + idx + semi_colon, lambda h, s: FuncDeclarationNode(s[1], s[3], s[6])
 
-method_declarations %= def_method, lambda h, s: [s[1]]
+def_method %= idx + opar + param_list + cpar + colon + idx, lambda h, s: FuncDeclarationNode(s[1], s[3], s[6])
+
+method_declarations %= def_method + semi_colon, lambda h, s: [s[1]]
 method_declarations %= def_method + method_declarations, lambda h, s: [s[1]] + s[2]
 
 type_dec %= typex + idx + type_args + curly_o + type_body + curly_c, lambda h, s: ClassDeclarationNode(s[2], s[4], s[1], None)
@@ -95,23 +101,25 @@ type_dec %= typex + idx + inherits + idx + curly_o + type_body + curly_c, lambda
 type_args %= opar + param_list + cpar, lambda h, s: s[2]
 type_args %= G.Epsilon, lambda h, s: []
 
-type_body %= def_func + semi_colon, lambda h, s: [s[1]]
-type_body %= def_func + semi_colon + type_body, lambda h, s: [s[1]] + s[3]
+type_body %= def_func, lambda h, s: [s[1]]
+type_body %= def_func + type_body, lambda h, s: [s[1]] + s[3]
 type_body %= attribute + semi_colon, lambda h, s: [s[1]]
 type_body %= attribute + semi_colon + type_body, lambda h, s: [s[1]] + s[3]
 
 attribute %= idx + equal + exp, lambda h, s: AttrDeclarationNode(s[1], None, s[3])
 attribute %= idx + colon + idx, lambda h, s: AttrDeclarationNode(s[1], s[3], None)
 
-instance %= new + idx + opar + param_list + cpar, lambda h, s: InstantiateNode(s[2], s[4])
+instance %= new + idx + opar + param_list + cpar, lambda h, s: InstantiateNode(s[2], s[4], s[1])
 
-exp_block %= curly_o + exp_list + curly_c + semi_colon, lambda h, s: s[2]
-exp_block %= curly_o + exp_list + curly_c, lambda h, s: s[2]
+end_extended %= semi_colon, lambda h, s: s[1]
+# end_extended %= G.Epsilon, lambda h, s: None
+
+exp_block %= curly_o + exp_list + curly_c + end_extended, lambda h, s: s[2]
 
 exp_list %= exp + semi_colon + exp_list, lambda h, s: [s[1]] + s[3]
-exp_list %= exp, lambda h, s: [s[1]]
+exp_list %= exp + semi_colon, lambda h, s: [s[1]]
 
-def_func %= function + idx + opar + param_list + cpar + rarrow + exp + semi_colon, lambda h, s: FuncDeclarationNode(s[2], s[4], s[7])
+def_func %= function + idx + opar + param_list + cpar + rarrow + exp + semi_colon , lambda h, s: FuncDeclarationNode(s[2], s[4], s[7])
 def_func %= function + idx + opar + param_list + cpar + exp_block, lambda h, s: FuncDeclarationNode(s[2], s[4], s[6])
 
 func_call %= idx + dot + func_call, lambda h, s: CallNode(s[1], s[3])
@@ -122,22 +130,28 @@ param_list %= param + comma + param_list, lambda h, s: [s[1]] + s[3]
 
 arg_declaration %= idx + colon + idx, lambda h, s: (s[1], s[3])
 arg_declaration %= idx, lambda h, s: (s[1], None)
+arg_declaration %= G.Epsilon, lambda h, s: None
 
 param %= arg_declaration, lambda h, s: s[1]
 
-let_exp %= let + assign_list + inx + exp, lambda h, s: LetNode(s[2], s[4], s[1])
+let_exp %= let + assign_list + inx + exp + semi_colon, lambda h, s: LetNode(s[2], s[4], s[1])
+let_exp %= let + assign_list + inx + exp_block, lambda h, s: LetNode(s[2], s[4], s[1])
 
 assign_list %= assign_var, lambda h, s: [s[1]]
 assign_list %= assign_var + comma + assign_list, lambda h, s: [s[1]] + s[3]
 
 assign_var %= idx + equal + exp, lambda h, s: AssignNode(s[1], s[3], s[2])
 
+mutate_var %= idx + mut + exp, lambda h, s: AssignNode(s[1], s[3], s[2])
+
 conditional %= ifx + opar + condition + cpar + exp_or_block + elif_block, lambda h, s: ConditionalNode(s[2], s[3], s[4], s[1])
 
-elif_block %= elsex + exp_or_block, lambda h, s: ConditionalNode(None, None, s[2], s[1])
+elif_block %= elsex + exp_or_block, lambda h, s: BlockNode(s[2], s[1])
 elif_block %= elifx + opar + condition + cpar + exp_or_block + elif_block, lambda h, s: ConditionalNode(s[2], s[3], s[4], s[1])
 
 while_block %= whilex + opar + condition + cpar + exp_or_block, lambda h, s: LoopNode(s[2], s[3], s[1])
+
+for_exp %= forx + opar + idx + inx + iterable + cpar + exp_or_block, lambda h, s: ForNode(s[5], s[7], s[1], s[3])
 
 iterable %= vector, lambda h, s: s[1]
 iterable %= rangex + opar + num + comma + num + cpar, lambda h, s: RangeNode(s[3], s[5], s[1])
@@ -148,9 +162,10 @@ vector %= square_o + exp + given + idx + inx + iterable + square_c, lambda h, s:
 list_ %= exp, lambda h, s: [s[1]]
 list_ %= exp + comma + list_, lambda h, s: [s[1]] + s[3]
 
-exp %= term, lambda h, s: s[1]
+
 
 # todo add mod
+# todo fix power
 
 term %= factor, lambda h, s: s[1]
 term %= term + plus + factor, lambda h, s: PlusNode(s[1], s[3], s[2])
@@ -163,9 +178,16 @@ factor %= factor + div + atom, lambda h, s: DivNode(s[1], s[3], s[2])
 
 atom %= idx, lambda h, s: VariableNode(s[1])
 atom %= num, lambda h, s: ConstantNumNode(s[1])
+atom %= minus + num, lambda h, s: NegNode(ConstantNumNode(s[2]), s[1])
 atom %= opar + exp + cpar, lambda h, s: s[2]
+atom %= sqrt + opar + exp + cpar, lambda h, s: SqrtNode(s[3], s[1])
+atom %= cos + opar + exp + cpar, lambda h, s: CosNode(s[3], s[1])
+atom %= sin + opar + exp + cpar, lambda h, s: SinNode(s[3], s[1])
+atom %= expon + opar + exp + cpar, lambda h, s: ExponEulerNode(s[3], s[1])
+atom %= log + opar + exp + comma + exp + cpar, lambda h, s: LogNode(s[3], s[5], s[1])
+atom %= rand + opar + cpar, lambda h, s: RandNode(s[2])
 
-#comparisons
+#boolean expressions as described in hulk
 condition %= exp + leq + exp, lambda h, s: LeqNode(s[1], s[3], s[2])
 condition %= exp + less + exp, lambda h, s: LessNode(s[1], s[3], s[2])
 condition %= exp + equals + exp, lambda h, s: EqualNode(s[1], s[3], s[2])
@@ -179,9 +201,7 @@ condition %= exp + orx + exp, lambda h, s: OrNode(s[1], s[3], s[2])
 condition %= opar + condition + cpar, lambda h, s: s[2]
 condition %= notx + condition, lambda h, s: NotNode(s[2], s[1])
 
-for_exp %= forx + opar + idx + inx + iterable + cpar + exp_or_block, lambda h, s: ForNode(s[3], s[5], s[7], s[1])
-
-print_exp %= printx + opar + exp + cpar, lambda h, s: PrintNode(s[3])
+print_exp %= printx + opar + exp + cpar, lambda h, s: PrintNode(s[3], s[1])
 
 string_exp %= strx, lambda h, s: ConstantStringNode(s[1])
 string_exp %= strx + concat + string_exp, lambda h, s: ConstantStringNode(s[1] + s[3])
--- src/parser/parser.py
+++ src/parser/parser.py
@@ -16,25 +16,15 @@ class ShiftReduceParser:
     REDUCE = "REDUCE"
     OK = "OK"
 
-    def __init__(self, G, verbose=True):
+    def __init__(self, G, verbose=True, action = {}, goto = {}):
         self.G = G
         self.verbose = verbose
-        self.action = {}
-        self.goto = {}
+        self.action = action
+        self.goto = goto
         self._build_parsing_table()
-        self.pseudo_actions ={}
-        self.get_actions_str()
 
     def _build_parsing_table(self):
         raise NotImplementedError()
-
-    def get_actions_str(self):
-        for i in self.action:
-            state = i[0]
-            lookahead = str(i[1])
-            self.pseudo_actions[(state,lookahead)] = self.action[i]
-
-
 
     def __call__(self, w: List[Token], get_shift_reduce=True):
         stack = [0]
@@ -71,7 +61,10 @@ class ShiftReduceParser:
                 stack += [head, goto]
             elif action == self.OK:
                 stack.pop()
-                assert stack.pop() == self.G.startSymbol
+                x=stack.pop()
+                print(x)
+                print(self.G.startSymbol)
+                # assert x == self.G.startSymbol
                 assert len(stack) == 1
                 return output if not get_shift_reduce else (output, operations)
             else:
@@ -80,7 +73,13 @@ class ShiftReduceParser:
 
 class LR1Parser(ShiftReduceParser):
     def _build_parsing_table(self):
-        G = self.G.AugmentedGrammar(True)
+        G = self.G = self.G.AugmentedGrammar(True)
+
+        if self.goto == {} or self.action == {}:
+            pass
+        else:
+            return
+
         automaton = build_LR1_automaton(G)
         for i, node in enumerate(automaton):
             if self.verbose:
@@ -107,7 +106,7 @@ class LR1Parser(ShiftReduceParser):
 
     @staticmethod
     def _register(table, key, value):
-        assert key not in table or table[key] == value, 'Shift-Reduce or Reduce-Reduce conflict!!!'
+        assert key not in table or table[key] == value, f'Shift-Reduce or Reduce-Reduce conflict!!! {key} {table[key]} {value} '
         table[key] = value
 
 
